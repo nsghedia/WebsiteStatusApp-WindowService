ad9eaffbee30c8477c4f9367a93a3a57aa0c86ce
Neha Ghedia <nsghedia@gmail.com>
2022-06-28 14:56:11 +0530

    Add Logging with serilog

--- WebsiteStatusApp/Program.cs
+++ WebsiteStatusApp/Program.cs
@@ -1,10 +1,34 @@
+using Serilog;
+using Serilog.Events;
 using WebsiteStatusApp;
 
-IHost host = Host.CreateDefaultBuilder(args)
+try
+{
+    Log.Information("Service started...");
+
+    Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Debug()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .Enrich.FromLogContext()
+        .WriteTo.File(@"G:\Study\Practicals\Logs.txt")
+        .CreateLogger();
+
+    IHost host = Host.CreateDefaultBuilder(args)
     .ConfigureServices(services =>
     {
         services.AddHostedService<Worker>();
     })
+    .UseSerilog()
     .Build();
 
-await host.RunAsync();
+    await host.RunAsync();
+
+}
+catch (Exception ex)
+{
+    Log.Fatal(ex, "There is some problem starting the service");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
